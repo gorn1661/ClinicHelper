3f59c55d84da7225a29e76cae4b3b34c844ea4fc
gorn1661 <mikolaj116@op.pl>
2017-05-08 23:48:02 +0200

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace DoctorsHelper
 {
@@ -20,8 +22,12 @@ namespace DoctorsHelper
     /// </summary>
     public partial class MainWindow : Window
     {
+        string _host = "localhost";
+        string _user = "root";
+        string _pass = "mikolaj";
+        string _database = "clinic";
+        string _connectionString;
 
-        string _database = "";
         string _name;
         string _surname;
         string _dateOfBirth;
@@ -31,11 +37,24 @@ namespace DoctorsHelper
         public MainWindow()
         {
             InitializeComponent();
+            if(checkDatabase(_database, _name, _surname, _dateOfBirth, _password) == true){
+                
+            }
         }
 
         private bool checkDatabase(string _database, string _name, string _surname, string _dateOfBirth, string _password)
         {
-            return false;
+            _connectionString = "SERVER=" + _host + ";DATABASE=" + _database + ";UID=" + _user + ";PASSWORD=" + _pass + ";";
+            try{
+                MySqlConnection connection = new MySqlConnection(_connectionString);
+                connection.Open();
+                connection.Close();
+                return true;
+            }catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return false;
+            }
         }
 
         private void logInB_Click(object sender, RoutedEventArgs e)
